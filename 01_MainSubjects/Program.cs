1b827695d6db6c6d992f9e9e2de25455b4a9832e
ByFatiq <ByFatiq@DESKTOP-Q6GQDVV>
2024-11-05 18:24:40 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Yazdırma Komutları
+            //Console.WriteLine("Merhaba Dünya");
+
+            //Console.Write("Selam");
+
+
+            //Console.WriteLine("*-*-*- Yemek Kategorileri *-*-*-");
+            //Console.WriteLine("1- Çorbalar");
+            //Console.WriteLine("1- Ana Yemekler");
+
+
+            //Console.WriteLine("*-*-*- Yemek Kategorileri *-*-*-");
+
+
+
+            #endregion
+
+            #region  Değişkenler  String - Veriables
+
+            //string name;
+            //name = "Fatih";
+
+            //Console.WriteLine(name);
+
+            //string pascalCase; //String tanımlaması
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;
+
+            //customerName = "Fatih";
+            //customerSurname = "ÇINAR";
+            //customerPhone = "0530 000 00 00";
+            //customerEmail = "fatihcnr@gmail.com";
+            //district = "Kadiköy";
+            //city = "İstanbul";
+
+
+
+            //Console.WriteLine("*-*-*--* Rezervasyon Kartı *-*-*-*");
+            //Console.WriteLine("");
+            //Console.WriteLine("Müşteri :"+ customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim :" + customerPhone);
+
+            //Console.WriteLine("------------");
+
+
+            //Console.Read();
+
+            #endregion
+
+            #region  Değişkenler  Integer - Veriables
+            //int number = 24;
+
+            //Console.WriteLine("number is: "+number);
+
+
+            int hamburgerPrice = 300;
+            int cokePrice = 35;
+
+            Console.WriteLine("*** Restoran Menü Fiyatı ****");
+
+            Console.WriteLine("Hamburger :" + hamburgerPrice);
+            Console.WriteLine("Kola :" + cokePrice);
+
+
+            #endregion
+        }
+    }
+}
---
+++ 02_Variables/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //double number = 4.82;
+
+
+
+            Console.WriteLine("***** CSharp Hava Yolları Bilgisi ******* ");
+            Console.WriteLine();
+
+
+            string passengerName, passengerSurname, passengerDistrict, passengerCity, passengerAge;
+
+            Console.WriteLine("Yolcu Adı");
+           passengerName = Console.ReadLine();
+
+            Console.Write("Yolcu Soyadı:");
+            passengerSurname = Console.ReadLine();
+
+            Console.Write("Yolcu Yaşı");
+            passengerAge = Console.ReadLine();
+
+            Console.Write("Yaşadığı il");
+            passengerCity = Console.ReadLine();
+
+            Console.Write("Yaşadığı ilçe");
+            passengerDistrict = Console.ReadLine();
+
+            Console.WriteLine("----------");
+
+            Console.WriteLine("Yolcu Adı Soyadı: " + passengerName, passengerSurname);
+            Console.WriteLine("Yolcu Yaşı :" + passengerAge);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
